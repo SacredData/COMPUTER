033f46115e075bda6b5aba7c351894665f6361af
agrathwohl <agrathwohl@protonmail.ch>
2015-10-01 14:10:57 -0400

    magic words module

---
+++ magic.py
@@ -0,0 +1,35 @@
+
+def magicWord(voca):
+    app_key = None
+    if any(substring in 'music' for substring in voca):
+        print "Magic word is music"
+        print "App is deadbeef"
+        app_key = "deadbeef"
+    elif any(substring in 'track' for substring in voca):
+        print "Magic word is track"
+        print "App is deadbeef"
+        app_key = "deadbeef"
+    elif any(substring in 'screenshot' for substring in voca):
+        print "Magic word is screenshot"
+        print "App is scrot"
+        app_key = "scrot"
+    elif any(substring in 'time' for substring in voca):
+        print "Magic word is time"
+        print "App is talking-clock"
+        app_key = "talking-clock"
+    # elif any(substring in 'todo' for substring in voca):
+    #     print "Magic word is todo"
+    #     print "App is sh"
+    elif any(substring in 'switch' for substring in voca):
+        print "Magic word is switch"
+        print "App is i3-msg"
+        app_key = "i3-msg"
+    elif any(substring in 'selfie' for substring in voca):
+        print "Magic word is selfie"
+        print "App is fswebcam"
+        app_key = "fswebcam"
+    elif any(substring in 'paste' for substring in voca):
+        print "Magic word is paste"
+        print "App is copyq"
+        app_key = "copyq"
+    return app_key
