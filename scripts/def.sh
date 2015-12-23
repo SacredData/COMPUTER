ec667a460604ef53e9d8933280bb6d062f5a8617
Andrew Grathwohl <andrew@sacreddata.io>
2015-12-22 21:19:37 -0500

    better error handling, queue management, messaging

--- scripts/def.sh
+++ scripts/def.sh
@@ -8,4 +8,4 @@ base="$(dirname $script)"
 
 wn "$WORD" -over | grep '1\.' > "$base/def_out"
 
-espeak -f "$base/def_out" --stdout | aplay -D 'card0'
+espeak -f "$base/def_out" --stdout | aplay -D 'card0'  # you may need to change 'card0' to a value appropriate for your ALSA config
